3a5dac43ef5411e945d14dcfe7f4d724fa6669cf
kamchatskiy <kamchatskiymma@gmail.com>
2024-10-28 19:34:44 +0300

    add deleterent handler

--- server/src/handlers/rent.go
+++ server/src/handlers/rent.go
@@ -7,6 +7,7 @@ import (
 	database "github.com/Kamchatskiy/NIS-CarRent/db"
 	"github.com/Kamchatskiy/NIS-CarRent/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetRents(ctx *gin.Context) {
@@ -19,4 +20,36 @@ func GetRents(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, rents)
+}
+
+func DeleteRent(ctx *gin.Context) {
+	db := database.GetDBFromContext(ctx)
+
+	rentID := ctx.Param("id")
+	if rentID == "" {
+		ctx.String(http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		ctx.Abort()
+		return
+	}
+
+	var rent models.Rent
+	if err := db.First(&rent, rentID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			ctx.String(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		} else {
+			log.Println(err)
+			ctx.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		}
+		ctx.Abort()
+		return
+	}
+
+	if err := db.Delete(&rent).Error; err != nil {
+		log.Println(err)
+		ctx.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		ctx.Abort()
+		return
+	}
+
+	ctx.String(http.StatusOK, http.StatusText(http.StatusOK))
 }
