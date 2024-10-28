038e1a02abf549f869a541301effdc888bd7fe84
kamchatskiy <kamchatskiymma@gmail.com>
2024-10-28 16:24:30 +0300

    update rent struct

--- server/src/models/rent.go
+++ server/src/models/rent.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type Rent struct {
-	ID        uint   `json:"id" gorm:"primaryKey"`
-	StartDate string `json:"start_date"`
-	EndDate   string `json:"end_date"`
-	Price     uint   `json:"price"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	ClientID  uint      `json:"client_id"`
+	CarID     uint      `json:"car_id"`
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+	Price     uint      `json:"price"`
 }
