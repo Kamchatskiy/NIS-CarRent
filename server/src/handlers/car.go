670fd9ac040f6677d06f9a3e3e46223befcf5987
kamchatskiy <kamchatskiymma@gmail.com>
2024-10-28 18:43:28 +0300

    add createcar handler

--- server/src/handlers/car.go
+++ server/src/handlers/car.go
@@ -7,6 +7,7 @@ import (
 	database "github.com/Kamchatskiy/NIS-CarRent/db"
 	"github.com/Kamchatskiy/NIS-CarRent/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetCars(ctx *gin.Context) {
@@ -21,4 +22,28 @@ func GetCars(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, cars)
+}
+
+func CreateCar(ctx *gin.Context) {
+	db := database.GetDBFromContext(ctx)
+
+	var car models.Car
+	if err := ctx.ShouldBindJSON(&car); err != nil {
+		log.Println(err)
+		ctx.String(http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		ctx.Abort()
+		return
+	}
+
+	var tempCar models.Car
+	if err := db.Where("model = ?", car.Model).First(&tempCar).Error; err == nil {
+		ctx.String(http.StatusConflict, "client already exists")
+		ctx.Abort()
+		return
+	} else if err != gorm.ErrRecordNotFound {
+		log.Println(err)
+		ctx.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		ctx.Abort()
+		return
+	}
 }
