3b22c7e2e27e038c2cf5277e9961186f1abe851c
kamchatskiy <kamchatskiymma@gmail.com>
2024-10-28 18:16:08 +0300

    small adjustments to client functionality handlers

--- server/src/handlers/client.go
+++ server/src/handlers/client.go
@@ -11,22 +11,25 @@ import (
 )
 
 func Register(ctx *gin.Context) {
-	db := database.MiddleWareDB(ctx)
+	db := database.GetDBFromContext(ctx)
 
 	var client models.Client
 	if err := ctx.ShouldBindJSON(&client); err != nil {
 		log.Println(err)
 		ctx.String(http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		ctx.Abort()
 		return
 	}
 
 	var tempClient models.Client
 	if err := db.Where("email = ?", client.Email).First(&tempClient).Error; err == nil {
 		ctx.String(http.StatusConflict, "client already exists")
+		ctx.Abort()
 		return
 	} else if err != gorm.ErrRecordNotFound {
 		log.Println(err)
 		ctx.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		ctx.Abort()
 		return
 	}
 
@@ -40,27 +43,10 @@ func Register(ctx *gin.Context) {
 }
 
 func GetClients(ctx *gin.Context) {
-	db := database.MiddleWareDB(ctx)
-
-	if err := ctx.ShouldBindHeader("Admin-Key"); err != nil {
-		log.Println(err)
-		ctx.String(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
-		return
-	}
-
-	key := ctx.GetHeader("Admin-Key")
-	adminKey, exists := ctx.Get("admin-key")
-	if !exists {
-		log.Fatalln("admin-key context error")
-		return
-	}
-	if adminKey != key {
-		ctx.String(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
-		return
-	}
+	db := database.GetDBFromContext(ctx)
 
 	var clients []models.Client
-	if err := db.Find(&clients); err != nil {
+	if err := db.Find(&clients).Error; err != nil {
 		log.Println(err)
 		ctx.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
