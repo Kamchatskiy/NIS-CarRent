3bc5c1f5b8a713c46498177ecc461d56f81fe8e8
kamchatskiy <kamchatskiymma@gmail.com>
2024-10-30 12:55:48 +0300

    create header

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -5,24 +5,25 @@ import { Cars } from "./pages/cars";
 import { Main } from "./pages/main";
 import { Order } from "./pages/order";
 import { MyOrders } from "./pages/my-orders";
-import { Footer } from "./footer";
+import { Footer } from "./components/footer";
 import { ThemeProvider } from "@emotion/react";
 import { MainTheme } from "./theme";
+import { Paper } from "@mui/material";
 
 export const App = () => {
   return (
     <>
       <ThemeProvider theme={MainTheme}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/cars" element={<Cars />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/my-orders" element={<MyOrders />} />
-          </Routes>
-        </BrowserRouter>
-        <Footer />
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/cars" element={<Cars />} />
+              <Route path="/order" element={<Order />} />
+              <Route path="/my-orders" element={<MyOrders />} />
+            </Routes>
+          </BrowserRouter>
+          <Footer />
       </ThemeProvider>
     </>
   );
